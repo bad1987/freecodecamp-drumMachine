ab5b8aed427db1841cc9d1268b1dd3be16bcc79b
badinfo1987 <badinfo1987@gmail.com>
2020-08-12 13:41:05 +0100

    Updates

---
+++ precache-manifest.94e21e04de9258c27aac34db71cdc250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe19be55df450926aa37963e7f32a1ee",
+    "url": "/freecodecamp-drumMachine/index.html"
+  },
+  {
+    "revision": "dfd2e2eeb075f6e1ad95",
+    "url": "/freecodecamp-drumMachine/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f0ebfdc472da2c4c69fc",
+    "url": "/freecodecamp-drumMachine/static/css/main.7d1eb767.chunk.css"
+  },
+  {
+    "revision": "dfd2e2eeb075f6e1ad95",
+    "url": "/freecodecamp-drumMachine/static/js/2.12de87f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/freecodecamp-drumMachine/static/js/2.12de87f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ebfdc472da2c4c69fc",
+    "url": "/freecodecamp-drumMachine/static/js/main.02bbf58f.chunk.js"
+  },
+  {
+    "revision": "500574adc9bae98001b7",
+    "url": "/freecodecamp-drumMachine/static/js/runtime-main.3d5e05bc.js"
+  }
+]);
